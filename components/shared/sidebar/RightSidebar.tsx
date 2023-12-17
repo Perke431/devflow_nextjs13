eb69bd810d3018f3eb07050974a2f978ed82b1ca
Marko Perovic <mperovic2210@gmail.com>
2023-12-17 20:14:34 +0100

    Webhooks part I

--- components/shared/sidebar/RightSidebar.tsx
+++ components/shared/sidebar/RightSidebar.tsx
@@ -22,8 +22,8 @@ const RightSidebar = () => {
             <div className='mt-16'>
                 <h3 className='h3-bold text-dark200_light900'>Popular Tags</h3>
                 <div className='mt-7 flex flex-col gap-4'>
-                    {popularTags.map(item => (
-                        <RenderTag key={item._id} tag={item} />
+                    {popularTags.map(tag => (
+                        <RenderTag key={tag._id} _id={tag._id} label={tag.label} />
                     ))}
                 </div>
             </div>
